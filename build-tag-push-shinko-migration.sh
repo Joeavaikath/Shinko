181c531cf76f6db0938a1b6fbd3a67756322ea83
Joseph A <joeavaikath@gmail.com>
2025-01-30 21:58:57 -0500

    Remove helper .sh files

--- build-tag-push-shinko-migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-podman build -t shinko-db-migration -f sql/Containerfile
-podman tag shinko-db-migration quay.io/joeavaik/shinko-db-migration
-podman push quay.io/joeavaik/shinko-db-migration
-podman image prune -a
--- build-tag-push-shinko.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-podman build -t shinko-app -f Containerfile
-podman tag shinko-app quay.io/joeavaik/shinko-app 
-podman push quay.io/joeavaik/shinko-app
-podman image prune -a
